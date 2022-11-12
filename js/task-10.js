7a4ea06a0fb653eb9e0c4324ddc59cc56334d789
K-Dima-K <106476955+K-Dima-K@users.noreply.github.com>
2022-11-12 20:52:24 +0200

    update task-10

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,40 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const mainBox = document.querySelector('#boxes');
+const input = document.querySelector('input');
+const create = document.querySelector('button[data-create]');
+const destroy = document.querySelector('button[data-destroy]');
+
+let amount = 0;
+const getAmount = event => {
+  amount = event.target.value;
+};
+
+function createBoxes(amount) {
+  let boxSize = 30;
+  mainBox.innerHTML = '';
+  for (let i = 0; i < amount; i += 1) {
+    const newBoxes = document.createElement('div');
+
+    boxSize += 10 * i;
+
+    newBoxes.style.width = `${boxSize}px`;
+    newBoxes.style.height = `${boxSize}px`;
+    newBoxes.style.backgroundColor = getRandomHexColor();
+    mainBox.append(newBoxes);
+  }
+}
+
+const onClickCreate = () => {
+  createBoxes(amount);
+};
+
+const onClickDestroy = () => {
+  mainBox.innerHTML = '';
+};
+
+input.addEventListener('change', getAmount);
+create.addEventListener('click', onClickCreate);
+destroy.addEventListener('click', onClickDestroy);
